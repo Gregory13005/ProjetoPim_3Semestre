47052272c378098c930be3d6a2417f21963ac84f
Gregory <lucasgre.1305@gmail.com>
2023-05-24 17:49:53 -0300

    Projeto sem as Interfaces

---
+++ ProjetoPim_3Semestre/Reajuste.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPim_3semestre
+{
+    public class Reajuste
+    {
+        public double Meed(double salario, double reajuste,ref double salarion)
+        {
+
+            reajuste = salario * reajuste;
+            salarion = salario + reajuste;
+            
+
+            return salarion;
+        }
+
+    }
+}
---
+++ ProjetoPim_3Semestre/Validador.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProjetoPim_3semestre
+{
+
+    public class Validador
+    {
+
+        static Dictionary<string, string> users = new Dictionary<string, string>();
+
+        public bool Validado(string login, string senha)
+        {
+            if (users.ContainsKey(login))
+            {
+                return users[login] == senha;
+            }
+
+            return false;
+        }
+
+        public void CadastroU(string login, string senha)
+        {
+            string nome, matricula,numero;
+            
+
+            if (!users.ContainsKey(login))
+            {
+                Console.WriteLine("Digite seu Nome:");
+               nome =  Console.ReadLine();
+                Console.WriteLine("Digite sua Matricula:");
+               matricula = Console.ReadLine() ;
+                Console.WriteLine("Digite seu Número(Apenas Números):");
+                numero = Console.ReadLine();
+
+                Console.WriteLine("Seus Dados São:");
+                Console.WriteLine("Nome:" + nome);
+                Console.WriteLine("Matricula:" + matricula);
+                Console.WriteLine("Número:" + numero);
+
+
+
+                users.Add(login, senha);
+            }
+        }
+
+    }
+}
+
